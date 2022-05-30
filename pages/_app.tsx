97603cfac570f5f69de371cb81bdf02a0c797741
Emilia Frisell <emilia.frisell@hyperisland.se>
2022-05-30 14:06:31 +0200

    adding material ui to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,68 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import Layout from "../components/layout/Layout";
 
+import { CssBaseline } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+
+export let shiftkeyTheme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        main: "#349082",
+      },
+      background: {
+        default: "#F0F0F0",
+      },
+      text: {
+        primary: "#777777",
+      },
+    },
+    typography: {
+      fontFamily: ["Catamaran", "sans-serif"].join(","),
+      fontSize: 15,
+      h1: {
+        fontSize: "2.2rem",
+        fontWeight: 800,
+      },
+      h2: {
+        fontSize: "1.5rem",
+        fontWeight: 800,
+        color: "#222328",
+      },
+      button: {
+        textTransform: "none",
+        fontWeight: 700,
+      },
+    },
+    // components: {
+    //   MuiListItemText: {
+    //     variants: [
+    //       {
+    //         props: {},
+    //         style: {
+    //           fontFamily: ["Montserrat", "sans-serif"].join(","),
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
+  })
+);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    // <ChakraProvider>
+    <ThemeProvider theme={shiftkeyTheme}>
+      <CssBaseline />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </ChakraProvider>
+    </ThemeProvider>
+    // </ChakraProvider>
   );
 }
 export default MyApp;
